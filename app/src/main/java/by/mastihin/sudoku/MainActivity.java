3363c328cec4fbfec9b3da46d90aba5666aedfb8
TheZebraz <andrew.evtukhov@gmail.com>
2018-03-09 16:07:59 +0300

    Sudoku view added

--- app/src/main/java/by/mastihin/sudoku/MainActivity.java
+++ app/src/main/java/by/mastihin/sudoku/MainActivity.java
@@ -2,8 +2,10 @@ package by.mastihin.sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
+import android.support.v7.widget.GridLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initGrid();
+        int[][] sudoku = new int[9][9];
+
+        showGrid(sudoku);
     }
 
-    private void initGrid() {
-        GridView gridView = findViewById(R.id.grid);
+    private void showGrid(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        GridLayout gridLayout = findViewById(R.id.table);
+        gridLayout.setColumnCount(columns);
+        gridLayout.setRowCount(rows);
+
+        for (int i = 0, k = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++, k++) {
+                matrix[i][j] = k;
+                GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f),
+                        GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f));
+                TextView item = (TextView) LayoutInflater.from(this).inflate(R.layout.item_grid, null);
+                item.setText(Integer.toString(matrix[i][j]));
+                gridLayout.addView(item, layoutParams);
+                Log.d("HEIGHT", "showGrid: " + item.getMeasuredHeight());
+            }
+        }
     }
 }
